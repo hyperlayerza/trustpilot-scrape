13d06538b1a30e668842d636a7452560e06c383b
Adam Gliori <adam.gliori@hotmail.com>
2024-03-28 17:41:04 +0200

    Update trustpilot.py

--- trustpilot.py
+++ trustpilot.py
@@ -16,13 +16,13 @@ from_page = 1
 to_page = 50
 
 for i in range(from_page, to_page + 1):
-    response = requests.get(f"https://www.trustpilot.com/review/COMPANY_NAME_HERE?page={i}")
+    response = requests.get(f"https://www.trustpilot.com/review/hyperlayer.net")
     web_page = response.text
     soup = BeautifulSoup(web_page, "html.parser")
 
-    for review in soup.find_all(class_ = "paper_paper__1PY90 paper_square__lJX8a card_card__lQWDv card_noPadding__D8PcU styles_cardWrapper__LcCPA styles_show__HUXRb styles_reviewCard__9HxJJ"):
+    for review in soup.find_all(class_ = "paper_paper__1PY90 paper_outline__lwsUX card_card__lQWDv card_noPadding__D8PcU styles_reviewCard__hcAvl"):
         # Review titles
-        review_title = review.find(class_ = "typography_typography__QgicV typography_h4__E971J typography_color-black__5LYEn typography_weight-regular__TWEnf typography_fontstyle-normal__kHyN3 styles_reviewTitle__04VGJ")
+        review_title = review.find(class_ = "typography_heading-xxs__QKBS8 typography_appearance-default__AAY17")
         review_titles.append(review_title.getText())
 
         # Review dates
@@ -46,7 +46,7 @@ for i in range(from_page, to_page + 1):
         review_ratings.append(review_rating["alt"])
         
         # When there is no review text, append "" instead of skipping so that data remains in sequence with other review data e.g. review_title
-        review_text = review.find(class_ = "typography_typography__QgicV typography_body__9UBeQ typography_color-black__5LYEn typography_weight-regular__TWEnf typography_fontstyle-normal__kHyN3")
+        review_text = review.find(class_ = "typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn")
         if review_text == None:
             review_texts.append("")
         else:
@@ -58,3 +58,6 @@ for i in range(from_page, to_page + 1):
 # Create final dataframe from lists
 df_reviews = pd.DataFrame(list(zip(review_titles, review_dates_original, review_dates, review_ratings, review_texts, page_number)),
                 columns =['review_title', 'review_date_original', 'review_date', 'review_rating', 'review_text', 'page_number'])
+
+# Save DataFrame to JSON file
+df_reviews.to_json('trustpilot_reviews.json', orient='records', indent=4)
